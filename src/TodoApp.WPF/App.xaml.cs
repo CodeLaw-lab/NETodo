4abc97e2b7c2730bb192f448d7f7afaee68b5c92
CodeLaw-lab <barabekus80lvl@proton.me>
2025-12-08 00:18:58 +0300

    Last update after first launch.

--- src/TodoApp.WPF/App.xaml.cs
+++ src/TodoApp.WPF/App.xaml.cs
@@ -1,14 +1,52 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using System.IO;
 using System.Windows;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-namespace TodoApp.WPF
+namespace TodoApp.WPF;
+
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+   private readonly IHost _host;
 
+   public App()
+   {
+      _host = Host.CreateDefaultBuilder()
+         .ConfigureAppConfiguration((context, config) =>
+         {
+            config.SetBasePath(Directory.GetCurrentDirectory());
+            config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+         })
+         .ConfigureServices((context, services) =>
+         {
+            ConfigureServices(services, context.Configuration);
+         })
+         .Build();
+   }
+
+   private void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+   {
+      services.AddSingleton<MainWindow>();
+   }
+
+   protected override async void OnStartup(StartupEventArgs e)
+   {
+      await _host.StartAsync();
+
+      var mainWindow = _host.Services.GetRequiredService<MainWindow>();
+      mainWindow.Show();
+
+      base.OnStartup(e);
+   }
+
+   protected override async void OnExit(ExitEventArgs e)
+   {
+      using (_host)
+      {
+         await _host.StopAsync(TimeSpan.FromSeconds(5));
+      }
+
+      base.OnExit(e);
+   }
 }
