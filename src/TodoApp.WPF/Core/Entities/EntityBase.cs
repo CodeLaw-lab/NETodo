e2d3019cd6c531ac1dc9a2c2f4b3e16960b4a815
CodeLaw-lab <barabekus80lvl@proton.me>
2025-12-08 00:24:54 +0300

    Creation of the first entities.

---
+++ src/TodoApp.WPF/Core/Entities/EntityBase.cs
@@ -0,0 +1,65 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoApp.WPF.Core.Entities;
+
+public abstract class EntityBase : INotifyPropertyChanged
+{
+   private int _id;
+   [Key]
+   public int Id
+   {
+      get => _id;
+      set
+      {
+         _id = value;
+         OnPropertyChanged(nameof(Id));
+      }
+   }
+
+   private DateTime _createdAt = DateTime.UtcNow;
+   public DateTime CreatedAt
+   {
+      get => _createdAt;
+      set
+      {
+         _createdAt = value;
+         OnPropertyChanged(nameof(CreatedAt));
+      }
+   }
+
+   private DateTime _modifiedAt = DateTime.UtcNow;
+   public DateTime ModifiedAt
+   {
+      get => _modifiedAt;
+      set
+      {
+         _modifiedAt = value;
+         OnPropertyChanged(nameof(ModifiedAt));
+      }
+   }
+
+   private bool _isDeleted;
+   public bool IsDeleted
+   {
+      get => _isDeleted;
+      set
+      {
+         _isDeleted = value;
+         OnPropertyChanged(nameof(IsDeleted));
+      }
+   }
+
+   public event PropertyChangedEventHandler? PropertyChanged;
+
+   protected virtual void OnPropertyChanged(string propertyName)
+   {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+   }
+
+   protected EntityBase()
+   {
+      CreatedAt = DateTime.UtcNow;
+      ModifiedAt = DateTime.UtcNow;
+   }
+}
---
+++ src/TodoApp.WPF/Core/Enums/Priority.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoApp.WPF.Core.Enums;
+
+public enum Priority
+{
+   Low = 0,
+   Medium = 1,
+   High = 2,
+   Critical = 3
+}
